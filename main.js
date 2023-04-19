a07502fcb3d924f6621cecba43ce484132ec748a
MarshallJavierNossaChanagaCAMPUS <infinitymc777@gmail.com>
2023-04-19 06:03:04 -0500

    feat: Propiedad numerica "MAX_VALUE"

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Propiedades de "Numbers"
 
-//Propiedad NEGATIVE_INFINITY, esta regresa como resultado "-Infinity"
+//Propiedad MAX_VALUE, esta regresa como resultado el numero mas alto que puede representar Javascript, el resultado esperado es "1.7976931348623157e+308"
 
-let num = Number.NEGATIVE_INFINITY;
+let num = Number.MAX_VALUE;
 
 console.log(num);
