90017dc1e8589cbed6aeabfe8ef97a0e9783ea0c
MarshallJavierNossaChanagaCAMPUS <infinitymc777@gmail.com>
2023-04-19 06:14:39 -0500

    feat: Metodo numerico "toFixed"

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //Metodos de "Numbers"
 
-//Metodo toExponential, este metodo regresa una cadena de texto representando el numero en su notacion exponencial, esto se utiliza para representar numeros muy grandes o muy pequeños de manera compacta
+//Metodo toFixed, este metodo redondea un numero y devuelve una cadena de texto de texto con un numero especifico de decimales
 
-let num = 120802;
+let num = 3.14159265359;
 
-console.log(num.toExponential(2));
+console.log(num.toFixed(2));
 
-//el resultado esperado es "1.21e+5"
+//el resultado esperado es "3.14"
